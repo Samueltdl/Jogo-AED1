ee892b907b0a2e64cad0fad2c50b0ce9da6b791b
Vitor De Ávila Freitas <108691011+vitorfreitasz@users.noreply.github.com>
2022-11-30 20:00:48 -0300

    .

--- jogo.py
+++ jogo.py
@@ -1,12 +1,12 @@
 import graphics as gf
 from time import sleep
 
-win = gf.GraphWin('navezinha piu piu', 700, 800)
+win = gf.GraphWin('navezinha piu piu', 700, 800, autoflush=False)
 
 
 
 def nave():   
-    nave = gf.Image(gf.Point(350, 500), 'nave.png')
+    nave = gf.Image(gf.Point(350, 500), 'img/nave.png')
     return nave
 
 x = nave()
@@ -17,7 +17,7 @@ while True:
     if direcao == 'a':
         x.move(-15, 0)
     
-    elif direcao == 'd':
+    dif direcao == 'd':
         x.move(15, 0)
 
 
