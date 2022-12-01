13bbca65a05a1426b72f2a03974e551d31d80983
Vitor De Ávila Freitas <108691011+vitorfreitasz@users.noreply.github.com>
2022-11-30 21:30:07 -0300

    .

--- jogo.py
+++ jogo.py
@@ -3,23 +3,30 @@ from time import sleep
 
 win = gf.GraphWin('navezinha piu piu', 700, 800, autoflush=False)
 
-background = gf.Image(gf.Point(350, 400), 'img/background.png')
-background.draw(win)
+'''background = gf.Image(gf.Point(350, 400), 'img/background.png')
+background.draw(win)'''
 
-nave = gf.Image(gf.Point(350, 750), 'img/nave.png')
+nave = gf.Image(gf.Point(350, 745), 'img/nave.png')
+hitbox= gf.Circle(gf.Point(350, 750), 40)
 nave.draw(win)
+hitbox.draw(win)
 
 while True:
+    x = gf.Point(50, 750)
     direcao = win.checkKey()
-    lado= win.checkKey()
     if direcao == 'a':
+        if hitbox.getCenter() == x:
+            print('foiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+            pass
+        hitbox.move(-5,0)
         nave.move(-5, 0)
     if direcao == 'd':
+        hitbox.move(5,0)
         nave.move(5, 0)
-    if lado == 'a':
-        nave.move(-5,0)
-    if lado == 'd':
-        nave.move(5,0)
+
+
+    print(hitbox.getCenter())
+
 
 
 
